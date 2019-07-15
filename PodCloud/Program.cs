14b15e04559f35e71a2d759b7c4cce6603e0e526
Thomas Copeland <talan.copeland@gmail.com>
2019-07-15 20:53:22 +1000

    initial commit

---
+++ PodCloud/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PodCloud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
